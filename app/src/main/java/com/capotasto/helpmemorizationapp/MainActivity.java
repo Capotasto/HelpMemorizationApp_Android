7d0f90c7f9f4be655a7a0b4a720292395cd1cce6
capotasto <rocker519804@gmail.com>
2015-10-28 22:49:25 -0700

    add list nnd button

--- app/src/main/java/com/capotasto/helpmemorizationapp/MainActivity.java
+++ app/src/main/java/com/capotasto/helpmemorizationapp/MainActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+
+        final ListView listView = (ListView) findViewById(R.id.main_list);
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"
+        };
+
+        ArrayList<String> list = new ArrayList<>();
+        for(String value : values){
+            list.add(value);
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+
     }
 
     @Override
@@ -33,5 +54,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+        ADD WORD Button is clicked
+     */
+    public void addNewWord(View view){
+
     }
 }
